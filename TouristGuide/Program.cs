2e77388fb705e837f1913d3ff71064112638e388
sanahaju <sanahaju123@gmail.com>
2022-10-03 16:29:09 +0530

    Add project files.

---
+++ TouristGuide/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using TouristGuide.DataLayer;
+
+namespace TouristGuide
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            IHost host = BuildWebHost(args);
+            await host.RunAsync();
+        }
+
+        public static IHost BuildWebHost(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(Web => {
+                    Web.UseStartup<Startup>()
+                      .UseConfiguration(Configuration)
+                      .UseSerilog();
+                })
+                .Build();
+    }
+}
